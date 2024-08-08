83e85e449e1593fb51ee9de69f14b923fe8450fb
No-IT-Aavding <hahutari@gmail.com>
2024-08-08 21:37:24 +0530

    Server Started

--- server/server.ts
+++ server/server.ts
@@ -1,2 +1,7 @@
 import { app } from "./app";
-require(".dotenv").config();
+require("dotenv").config();
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is Live on " + process.env.PORT);
+})
