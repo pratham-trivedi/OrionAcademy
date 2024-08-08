3e559ccac9c079d81a85e1f0fde1e6ce82624390
No-IT-Aavding <hahutari@gmail.com>
2024-08-08 23:52:49 +0530

    Error Handler Added

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ require('dotenv').config();
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import {ErrorMiddleware} from "./middleware/error";
 
 
 app.use(express.json({limit: "50mb"}));
@@ -25,4 +26,6 @@ app.all("*", (req:Request, res:Response, next:NextFunction) => {
     const err = new Error(`Route ${req.originalUrl} not valid or doesnt exist`) as any;
     err.statusCode = 404;
     next(err);
-})
+});
+
+app.use(ErrorMiddleware);
